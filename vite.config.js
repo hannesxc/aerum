651634fe96f4ce3038eb4b9a538bbfc2ea5fedf7
hannesxc <chakravortyaditya@gmail.com>
2023-01-03 00:11:36 +0530

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://hannesxc.github.io/aerum'
+  base: "/aerum/",
+  plugins: [react()]
 })
